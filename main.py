efded7df562712353187274a3f644a400c3fb51e
Berenjian1984 <berenjian84@gmail.com>
2025-06-06 16:37:03 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-TOKEN = os.environ.get("BOT_TOKEN")
+TOKEN = os.environ.get("7988347665:AAFj7a0BA-VekPcMYat3qr9PuMApF5w8yYA")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! من ربات تلگرام شما هستم. آماده‌ام :)")
